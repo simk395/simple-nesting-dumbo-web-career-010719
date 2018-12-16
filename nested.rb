7534e7ce40943ad3f589482d109a8a9cdd9c63b1
Simon Kong <simon.kong95@gmail.com>
2018-12-16 03:44:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+		 programmer_hash[:grace_hopper][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz

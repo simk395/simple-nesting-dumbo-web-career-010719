8d9194708df74a9d89e238db6eaf686acc397922
Simon Kong <simon.kong95@gmail.com>
2018-12-16 04:13:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-  
+
 
 end
 
@@ -130,5 +130,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		
+
 end

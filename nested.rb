811a988576a6830ae2caf49d5f61ba1850afaa3b
Simon Kong <simon.kong95@gmail.com>
2018-12-16 04:13:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
 
 }
 
-programmer_hash[:yukihiro_matsumoto] ={
+programmer_hash[:yukihiro_matsumoto] = {
 	known_for: "Ruby",
 	languages: ["LISP", "C"]
 }
@@ -110,8 +110,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
 
 end
 
@@ -131,5 +130,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+		
 end
